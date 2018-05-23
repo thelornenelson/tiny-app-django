5719dc8d7d21ddbbbef7f37b844671d36ea8b934
Andrew Barclay <andrewkbarclay@gmail.com>
2018-05-23 22:17:41 +0000

    added initial url listing template

--- urls/views.py
+++ urls/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Url
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the urls index.")
+    all_urls = Url.objects.order_by('-pub_date')
+    context = {
+        'all_urls': all_urls,
+    }
+    return render(request, 'urls/index.html', context)
 
 def short_url(request, short_url):
     response = "You would normally be redirected to the long url associated with %s"
